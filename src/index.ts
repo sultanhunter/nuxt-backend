4eb8b8ab8ee1cde1767b88bf563f846cf214232a
sultan <sultanibneusman0@gmail.com>
2024-02-25 02:46:25 +0530

    frontend origin change

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ const app = express();
 const port = process.env.PORT || 8000;
 
 const corsOptions = {
-    origin: "http://localhost:3000",
+    origin: "https://yocket-nuxt.netlify.app",
     optionsSuccessStatus: 200,
     credentials: true,
 };
